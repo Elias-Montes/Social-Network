23fbf8aa837eb3c6f35d5aa67393e111e10636a7
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-09-29 15:17:02 -0700

    added index for routes and api

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+const router =require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughts = require('./thoughts');
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughts);
+module.exports = router;
