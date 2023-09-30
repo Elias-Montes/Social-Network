aeb40c08d29cceb9f7f322c8865d4e473d459695
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-09-29 20:30:38 -0700

    fixed a err in routes index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const api = require('/api');
+const api = require('./api');
 router.use('/api', api);
 router.use((req, res) => {
     res.status(404).send('<h1>Error</h1>')
